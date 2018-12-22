43561041197bfe46b822d2db5cdcc5d754d3cb5b
vektorel <vektorel@lab9-4>
2018-12-22 13:23:13 +0300

    22.12.2018: ders

---
+++ src/murat/obj/Arac.java
@@ -0,0 +1,18 @@
+package murat.obj;
+
+public class Arac {
+	private int tekerlekSayisi;
+	public boolean koltuk;
+	
+	public Arac() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setTekerlekSayisi(int tekerlekSayisi) {
+		this.tekerlekSayisi = tekerlekSayisi;
+	}
+	
+	public int getTekerlekSayisi() {
+		return tekerlekSayisi;//baska bir tekerlek sayisi olmadigi icin this.tekerlekSayisi ile ayni anlamda
+	}
+}
---
+++ src/murat/obj/Runner.java
@@ -0,0 +1,11 @@
+package murat.obj;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Arac temp = new Arac();
+		temp.koltuk = true;
+	}
+
+}
